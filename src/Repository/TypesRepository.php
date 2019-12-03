261f7fb1212fe7a9eb8e20d2a7cde8abc647385c
alarai <wedge@atlanteans.net>
2019-12-03 21:49:54 +0100

    Updated librarires for fixing security issue

--- src/Repository/TypesRepository.php
+++ src/Repository/TypesRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Types;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Types|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class TypesRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Types::class);
     }
